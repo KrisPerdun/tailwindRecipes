1ba98eec920796f006a1988d57d82566f90a551c
Kristopher Perdun <krisperdun@gmail.com>
2022-07-12 16:12:53 -0700

    SecondCommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#FF6363',
-        secondary: {
-          100: '#E2E2D5',
-          200: '#888883',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
